59f03bb5b6f1b459e927cb1bf0d601da3068a56c
Jean-Guillaume Buret <jg.buret@gmail.com>
2016-05-31 10:18:26 +0200

    Command::Help added a private method to get the list of the available commands

--- app/command/help.rb
+++ app/command/help.rb
@@ -7,5 +7,21 @@ class Command
     def init
       @update_core_state = true
     end
+
+    private
+
+    def available_commands
+      classes = self.class.constants.map { |c| to_underscored_name(c.to_s) }
+    end
+
+    def to_underscored_name(class_name)
+      word = class_name.split('::').last
+      word.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+      word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
+      word.tr!("-", "_")
+      word.downcase!
+      word
+  end
+
   end
 end
