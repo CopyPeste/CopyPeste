b5f32b6e0d11bc4d48e30f1bd92582a440c273f9
Jean-Guillaume Buret <jg.buret@gmail.com>
2016-06-01 16:50:48 +0200

    >UNSTABLE< Starting to propagate the namespace CopyPeste. Reviewed the Require functionnality.

--- lib/copy_peste.rb
+++ lib/copy_peste.rb
@@ -1,62 +1,19 @@
 require 'yaml'
-# require 'class_loader'
+require 'class_loader'
+require_relative 'ext/string'
+require_relative 'copy_peste/require'
 
-class CpRequire
+module CopyPeste
 
-  def self.base_path
-    File.expand_path(File.dirname(__FILE__))
-  end
+  module_function
 
-  def self.modules file_path
-    require File.expand_path(File.dirname(__FILE__) + "/modules/" + file_path)
-  end
-
-  def self.libs file_path
-    require File.expand_path(File.dirname(__FILE__) + "/libs/" + file_path)
-  end
-
-  def self.app file_path
-    require File.expand_path(File.dirname(__FILE__) + "/app/" + file_path)
-  end
-
-  def self.config file_path
-    require File.expand_path(File.dirname(__FILE__) + "/config/" + file_path)
-  end
-end
-
-CpRequire.app 'core/core.rb'
-
-class CopyPeste
-
-  def self.run
-    instance.run
+  def run(dir: '/', file: 'copy_peste.yml')
+    config = YAML::load_file(File.join(dir, file)) if File.exists? (dir+file)
+    puts !!config
+    # core = Core.new config
+    # core.start
     self
   end
-
-  def run
-    @core.start
-  end
-
-  private
-
-  def self.new(*args, &blk)
-    super(*args, &blk)
-  end
-
-  def self.instance
-    @@instance ||= new
-  end
-
-  def initialize
-    @conf = load_config
-    @core = Core.new @conf
-  end
-
-  def load_config (dir = "/config/framework/", file = "default.yml")
-    dir = CpRequire.base_path + dir
-    YAML::load_file(File.join(dir, file)) if File.exists? (dir+file)
-  end
-
 end
 
 CopyPeste.run
---
+++ lib/ext/string.rb
@@ -0,0 +1,10 @@
+class String
+  def underscore
+    word = self.dup
+    word.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+    word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
+    word.tr!("-", "_")
+    word.downcase!
+    word
+  end
+end
