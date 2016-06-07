a475415f13ae9ce5f9ae654e630f83d70442da0c
Yann <yann.ferrere@epitech.eu>
2016-06-07 13:35:00 +0100

    This commit ref #-1, change Utils class to module.

--- lib/copy_peste/core/utils.rb
+++ lib/copy_peste/core/utils.rb
@@ -1,10 +1,14 @@
 module CopyPeste
   class Core
-    class Utils
-      def self.load_module (dir, file)
+    module Utils
+
+      module_function
+
+      def load_module (dir, file)
         loaded_mod = ModuleLoading::Loader.load File.join dir, file
         loaded_mod.__cp_init__
       end
+
     end
   end
 end
