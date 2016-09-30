30257b44fd83b8bd2ef0fea8ee89006c45de5078
JeremyPouyet <jeremy.pouyet@epiteh.eu>
2016-09-30 20:26:06 +0000

    This commit ref #984 - remove old use_levenshtein file and do not compare files with 100% similarities multiple times

--- public/modules/analysis/fdf/fdf.rb
+++ public/modules/analysis/fdf/fdf.rb
@@ -26,9 +26,9 @@ fdfAnalysisModule do
 
       def initialize
         @options = {
-          "e" => {
-            helper: "sort by extension.",
-            allowed: [0, 1],
+          "l" => {
+            helper: "Minimum levenshtein distance",
+            allowed: [*0..10],
             value: 1
           },
           "s" => {
@@ -73,23 +73,20 @@ fdfAnalysisModule do
       end
 
 
-      # Get files from the database that will be analysed
+      # Retrieve and sort files from database
+      # FIles are sorted by extension and by size
       #
-      # @Return [Array] return a file Array with the full file path :  Array[0] = /home/test/expemple.c
+      # @Return [Hash] Each entry is a file extension that maps to an array of files order by size
       def get_doc_to_analyse
         documents = {}
         query = {name: {"$nin" => @ignored_conf.ignored_ext}}
-        # get all extensions
         extensions = @mongo.get_data("Extension", query, nil)
-        # for each extension
         extensions.each do |extension|
           extension["_id"] = BSON::ObjectId.from_string extension["_id"]["$oid"]
           @show.call "\tRetrieving files with extension: '#{extension["name"]}'."
           query = {ext: extension["_id"], name: {"$nin" => @ignored_conf.ignored_files }}
-          # get all corresponding files
           files = @mongo.get_data(@c_file, query)
           documents[extension["name"]] = []
-          # save each file, formated, in the good extension
           files.each do |file|
             doc = to_doc(file)
             documents[extension["name"]] << doc if doc != nil
@@ -100,12 +97,12 @@ fdfAnalysisModule do
       end
       
 
-      # Open the two files and send there containt to fdupes algorithm
+      # Open files and compare their contents with the appropriate algorithm
       #
-      # @param [Array] File Array
-      # @param [Integer] Loop position
+      # @param [Hash] f1 represents the first file
+      # @param [Hash] f2 represents the second file
       # @return [Integer] Return the percentage of difference between files
-      def open_and_send(f1, f2)
+      def compare_files(f1, f2)
         begin
           file1 = IO.read f1[:path]
           file2 = IO.read f2[:path]
@@ -126,33 +123,30 @@ fdfAnalysisModule do
       end
 
       
-      # Send files to the fdupes algorithms
+      # Search for files duplicated according to user params
       # 
-      # @param [Array] File array containing levenshtein's results
+      # @param [Hash] Each entry is a file extension that maps to an array of files order by size
       def check_files_similarity(documents)
         duplicated_files = {}
-        others = {}
-        # for each extension
         documents.each do |extension, files|
           next if files.length < 2
           @show.call "\tSearching for duplication with extension: '#{extension}'."
-          # iterate all files to find duplications
-          (files.length - 1).times do |i|
-            next if files[i] == nil
-            f1 = files[i]
-            ((i + 1)..(files.length - 1)).each do |j|
+          files.each_with_index do |f1, index|
+            next if f1 == nil || index == files.length - 1
+            ((index + 1)..(files.length - 1)).each do |j|
               next if files[j] == nil
               f2 = files[j]
-              # stop loop if size is required but files aren't of the same size
               break if @options["s"][:value] == 1 && f1[:size] != f2[:size]
-              # allow little differences in filenames
-              next if Algorithms.levenshtein(f1[:name], f2[:name]) > 1
-              result = open_and_send(f1, f2)
+              next if Algorithms.levenshtein(f1[:name], f2[:name]) > @options["l"][:value]
+              result = compare_files(f1, f2)
               next if result == nil
               f3 = f2.clone
               f3[:similarity] = result
               if (@options["p"][:value] == 100 && result == 100) || (result >= @options["p"][:value])
-                files[j] = nil
+                # remove one of the file 100% duplicated
+                # 2 files 100% duplicated will also be duplicated with other files
+                # if sim(a,b) == 100 && sim(a, c) == 100 so sim(a, c) == 100
+                files[j] = nil if @options["p"][:value] == 100 && result == 100
                 if duplicated_files.key?(f1[:path])
                   duplicated_files[f1[:path]] << f3
                 else
@@ -167,13 +161,9 @@ fdfAnalysisModule do
 
 
       # Function used to initialize and run the fdf
-      # and get the list of file to analyse.
       def run(*)
         @show.call "Get all files from database..."
         files = get_doc_to_analyse
-        @show.call "Done."
-        @show.call "Sort files depending on options..."
-        #sorted_file = sort_files_with_rules files
         @show.call "Done."
         @show.call "Searching for duplicated files..."
         check_files_similarity files
